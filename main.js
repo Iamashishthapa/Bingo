e6a9416d8b2ee8fa5b26ed0a2ad4c406d5c22d0b
Dedsec <dedsec@localhost.localdomain>
2019-07-09 23:23:35 +0545

    In this program error is in puttting 15 number in 27 numbers V 2.0

--- main.js
+++ main.js
@@ -1,185 +1,192 @@
-var i,j;
-var arr=new Array(13);
-var hun=new Array(100);
-var pop=0,all=0;
-var rand9 = new Array(5);
-var rowNum = new Array(9);
-var count;
-var finalArray = new Array (27);
-var FLAG = 0;
+var i,j;//as counter......
+var arr=new Array(13);//in Random.....
+var randomFive=new Array(5);//in generate.....
+var all=0;//in Random1...
+var all1=0;//in unique...
+var a=0;//in tables()......
+var count1=0;//in unique......
+var randomNine=new Array(9);//in total.....
+var finalArray=new Array(27);//in total.....
+var a1;// in total........
+var maxValue=0;//in total
 
+//Calls table() function number of times...
 
-    //Calls table() function number of times...
-
-    function howMany(){ 
-            count = document.getElementById('loop').value;
-        for(var k=0; k<1 ;k++)
+function howMany()
+{ 
+    count = document.getElementById('loop').value;
+    for(var i=0; i<1 ;i++)
         {
             Random();
             Random1();
             generate();
-            //tables();
+            tables();
         }   
-    }
-    
-    //Generates 15 random numbers.........
+}
 
-    function Random()
-    { 
-        for(i=0;i<15;i++)
-        {
-            arr[i]=Math.floor((Math.random()*98)+1);          
-        }
-        Random1();
-    }
+ //Generates 15 random numbers.........
 
-    //Checks whether the Random() function numbers are unique.....
-
-
-    function Random1()
+function Random()
+{ 
+    for(i=0;i<15;i++)
     {
-        for(i=0;i<15;i++)
-        {
-            for(j=0;j<15;j++)
+        arr[i]=Math.floor((Math.random()*98)+1);          
+    }
+    Random1();
+}
+
+  //Checks whether the Random() function numbers are unique.....
+
+
+function Random1()
+{
+    for(i=0;i<15;i++)
+    {
+        for(j=0;j<15;j++)
             {
                 if(arr[i]==arr[j] && i!=j)
-                {
+                    {
                     Random();
                     break;
-                }
+                    }
             }
-        }
-    
-        all=0;
-        for(i=0;i<15;i++){
+    }
+    all=0;
+    for(i=0;i<15;i++)
+        {
             all = all + arr[i] + "<br>"; 
         }
-        document.getElementById('random').innerHTML="Random 15 numbers :"+"<br>"+all;
-        generate();
-        
-    }
+    document.getElementById('random').innerHTML="Random 15 numbers :"+"<br>"+all;  
+}
 
-    //creates table.......
-    function tables()
-    {
-        var table = document.getElementById("table");
-        var a = 0;
-        var row = 3;
-        var cols = 9;
-        //console.log( count);
+//creates table.......
+function tables()
+{
+    var table = document.getElementById("table");
+    var row = 3;
+    var cols = 9;
+    //console.log( count);
 
-        for (var i=0; i<row ; i++)
+    for (var i=0; i<row ; i++)
         {
-            var tr = document.createElement('tr');
-            
+            a=0;
+            var tr = document.createElement('tr'); 
             for(var j=0; j<cols ; j++)
-            {
-                var td = document.createElement('td');
-                if (a<=27){ 
-                    var text = document.createTextNode(finalArray[a]);
-                } 
-                a++;
-                td.appendChild(text);
-                tr.appendChild(td);
-            }
+                {
+                    var td = document.createElement('td');
+                    if (a<=27)
+                        { 
+                            var text = document.createTextNode(finalArray[a]);
+                        } 
+                    a++;
+                    td.appendChild(text);
+                    tr.appendChild(td);
+                }
             table.appendChild(tr);
         }
-        var textnode =document.createTextNode("**");
-        table.appendChild(textnode);
-    }
+    var textnode =document.createTextNode("**");
+    table.appendChild(textnode);
+}
 
-    document.getElementById('makeTable').addEventListener("click",makeTable);
+document.getElementById('makeTable').addEventListener("click",makeTable);
 //____________________________________________________________________________________________________________
 
-    var all1 = 0, count1,count2=0, maxValue=0,temp=0;
-    var a1=0;
+//genrates 5 numbers betn 0-8........
 
-    //genrates 5 numbers betn 0-8........
-    function generate(){
-        for( i=0; i<5; i++)
+function generate()
+{
+    for( i=0; i<5; i++)
         {
-            rand9[i]=0;
+            randomFive[i]=0;
         }
-        for( i=0; i<5; i++)
+    for( i=0; i<5; i++)
         {
-            rand9[i]=Math.floor((Math.random()*9));
-            if (i==4){
+            randomFive[i]=Math.floor((Math.random()*9));
+            if (i==4)
+            {
                 unique();
             }
         }
-        
-    }
-    
-    //Checks whether the generate() function numbers are unique.....
+}
 
-    function unique(){
-        count1 = 0;
+//Checks whether the generate() function numbers are unique.....
 
-        for(i=0;i<5;i++)
+function unique()
+{
+    count1 = 0;
+    for(i=0;i<5;i++)
         {
             for(j=0;j<5;j++)
-            {
-                if(rand9[i]== rand9[j] && i!=j)
                 {
-                    generate();
-                
+                    if(randomFive[i]== randomFive[j] && i!=j)
+                        {
+                            generate();
+                        }
                 }
-            }
             count1++;
-            
         }
-        all1=0;
-        // for(i=0;i<5;i++){
-        //     all1 = all1 + rand9[i] + "<br>"; 
-        // }
-        // document.getElementById('gen5').innerHTML="Random 15 numbers :"+"<br>"+all1;
-        if (count1==5){
-            total();    
-        } 
-    
-    }
-    
+    // all1=0;
+    // for(i=0;i<5;i++){
+    // all1 = all1 + randomFive[i] + "<br>"; 
+    // }
+    // document.getElementById('gen5').innerHTML="Random 15 numbers :"+"<br>"+all1;
+    if (count1==5)
+    {
+        total();    
+    } 
+}
 
-
-    //Stores 15 numbers in table in random order.........
-    function total(){ 
-        for ( i=0; i<9; i++)
+//Stores 15 numbers in table in random order.........
+function total()
+{ 
+    if(maxValue==0)
         {
-            rowNum[i] = '*';
+            a1=0;
         }
-        for ( i=0; i<5; i++)
+    else
         {
-            var b = rand9[i];
-            rowNum[b] = arr[a1];
-            var c= arr[a1];
-            console.log(a1);    
+            a1=5*(maxValue/9);
+        }
+    for ( i=0; i<9; i++)
+        {
+            randomNine[i] = '*';
+        }
+    for ( i=0; i<5; i++)
+        {
+            var b=randomFive[i];
+            randomNine[b] = arr[a1];
+            var c = arr[a1];  
             a1++;
         }
-        all1=0;
-        for(i=0;i<9;i++){
-            all1 = all1 + rowNum[i] + "<br>"; 
+    all1=0;
+    for(i=0;i<9;i++)
+        {   
+            if(i==0)
+                {
+                    all1=randomNine[i]+"<br>";
+                }
+            else
+                {
+                    all1 = all1 + randomNine[i] + "<br>"; 
+                }
         }
-        document.getElementById('gen5').innerHTML="Random 15 numbers :"+"<br>"+all1;
-        
-        if ( a1>=15)
-        {
-            a1 = 0;
-        }
-        for(var j = 0; j<9 ; j++ )
-            {
-                finalArray[maxValue] = rowNum[j]; 
-                maxValue ++ ;
-            }
-            //console.log('coming 3');
-            count2 ++;
-            if (count2 != 3){
-                //console.log('go');
-                //generate();
-            }
-            else{
-                maxValue=0;
-                count2=0;
-                console.log('finished');
-            }
-
+    document.getElementById('gen5').innerHTML="Random 15 numbers :"+"<br>"+all1;
+    if ( a1>=15)
+    {
+        a1 = 0;
     }
+    for(var j = 0; j<9 ; j++ )
+        {
+            finalArray[maxValue] = randomNine[j]; 
+            maxValue ++ ;
+        }
+        if (maxValue<=27)
+        {
+            generate();
+        }
+        else{
+            maxValue=0;
+            console.log('finished');
+        }
+
+}
